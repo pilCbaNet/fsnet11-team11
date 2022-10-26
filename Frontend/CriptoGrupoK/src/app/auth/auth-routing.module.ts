6a2247bd4289785bd5f96007bbbd22126a19c850
Facundo Montenegro <facumonte29@gmail.com>
2022-10-26 00:07:05 -0300

    agrega estilos

--- Frontend/CriptoGrupoK/src/app/auth/auth-routing.module.ts
+++ Frontend/CriptoGrupoK/src/app/auth/auth-routing.module.ts
@@ -4,6 +4,7 @@ import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
+
 const routes: Routes = [
   {
     path: '',
