9d41b859d8f21eba57adf437c21d3b40d58737a4
Francisco <franciscomauri47@gmail.com>
2022-11-01 14:42:04 -0300

    Fix responsive 1

--- Frontend/CriptoGrupoK/src/app/pages/about-us/about-us.component.ts
+++ Frontend/CriptoGrupoK/src/app/pages/about-us/about-us.component.ts
@@ -15,14 +15,14 @@ export class AboutUsComponent implements OnInit {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Francisco Aguirre', photo: '../../../assets/quienes-somos/Fotos/FRAN.jpg', gh: '', ln: '', ig: '', cols: 1, rows: 1 },
-          { title: 'Francisco Mauri', photo: '../../../assets/quienes-somos/Fotos/MAURI.jpeg', gh: 'https://github.com/franmauri47', ln: 'https://www.linkedin.com/in/francisco-m-a37898a8/', ig: 'https://www.instagram.com/fran_mauri/', cols: 1, rows: 1 },
-          { title: 'Sebastian Moreno', photo: '../../../assets/quienes-somos/Fotos/SEBA.jpg', gh: '', ln: '', ig: '', cols: 1, rows: 1 },
-          { title: 'Lucas Beaufils', photo: '../../../assets/quienes-somos/Fotos/LUCAS.jpg', gh: '', ln: '', ig: '', cols: 1, rows: 1 },
-          { title: 'Matías McNamara', photo: '../../../assets/quienes-somos/Fotos/MATI.jpg', gh: 'https://github.com/matiasmcni', ln: 'https://www.linkedin.com/in/matiasmcnamara/', ig: 'https://www.instagram.com/matiasmcni/', cols: 1, rows: 1 },
-          { title: 'Facundo', photo: '../../../assets/quienes-somos/Fotos/FACU.jpg', gh: '', ln: '', ig: '', cols: 1, rows: 1 }
+          { title: 'Francisco Aguirre', photo: '../../../assets/quienes-somos/Fotos/FRAN.jpg', gh: '', ln: '', ig: '', cols: 2, rows: 1 },
+          { title: 'Francisco Mauri', photo: '../../../assets/quienes-somos/Fotos/MAURI.jpeg', gh: 'https://github.com/franmauri47', ln: 'https://www.linkedin.com/in/francisco-m-a37898a8/', ig: 'https://www.instagram.com/fran_mauri/', cols: 2, rows: 1 },
+          { title: 'Sebastian Moreno', photo: '../../../assets/quienes-somos/Fotos/SEBA.jpg', gh: '', ln: '', ig: '', cols: 2, rows: 1 },
+          { title: 'Lucas Beaufils', photo: '../../../assets/quienes-somos/Fotos/LUCAS.jpg', gh: '', ln: '', ig: '', cols: 2, rows: 1 },
+          { title: 'Matías McNamara', photo: '../../../assets/quienes-somos/Fotos/MATI.jpg', gh: 'https://github.com/matiasmcni', ln: 'https://www.linkedin.com/in/matiasmcnamara/', ig: 'https://www.instagram.com/matiasmcni/', cols: 2, rows: 1 },
+          { title: 'Facundo', photo: '../../../assets/quienes-somos/Fotos/FACU.jpg', gh: '', ln: '', ig: '', cols: 2, rows: 1 }
         ];
-      } else{
+      } 
         return [
           { title: 'Francisco Aguirre', photo: '../../../assets/quienes-somos/Fotos/FRAN.jpg', gh: '', ln: '', ig: '', cols: 1, rows: 1 },
           { title: 'Francisco Mauri', photo: '../../../assets/quienes-somos/Fotos/MAURI.jpeg', gh: 'https://github.com/franmauri47', ln: 'https://www.linkedin.com/in/francisco-m-a37898a8/', ig: 'https://www.instagram.com/fran_mauri/', cols: 1, rows: 1 },
@@ -31,7 +31,7 @@ export class AboutUsComponent implements OnInit {
           { title: 'Matías McNamara', photo: '../../../assets/quienes-somos/Fotos/MATI.jpg', gh: 'https://github.com/matiasmcni', ln: 'https://www.linkedin.com/in/matiasmcnamara/', ig: 'https://www.instagram.com/matiasmcni/', cols: 1, rows: 1 },
           { title: 'Facundo', photo: '../../../assets/quienes-somos/Fotos/FACU.jpg', gh: '', ln: '', ig: '', cols: 1, rows: 1 }
         ];
-      }      
+           
     })
   );
   constructor(private breakpointObserver: BreakpointObserver) { }
