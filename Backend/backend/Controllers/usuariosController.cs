30ef59bc53edcf0a65effccc551fee45ba465471
lucas <labpersonal55@gmail.com>
2022-12-09 13:53:35 -0300

    backend first push

---
+++ Backend/backend/Controllers/usuariosController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using cryptoAppAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace cryptoAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosController : ControllerBase
+    {
+        // GET: api/<usuariosController>
+        [HttpGet]
+        public List<Usuario> Get()
+        {
+            using (var db = new cryptoDbContext()) 
+            {
+               return db.Usuarios.ToList();
+            }
+        }
+
+        // GET api/<usuariosController>/5
+        [HttpGet("mail")]
+        public Usuario Get(string mail, string contraseña)
+        {
+            using (var db = new cryptoDbContext()) 
+            {
+                Usuario user = new Usuario();
+
+                var resp = db.Usuarios.FirstOrDefault(u => u.Mail == mail && u.Contraseña == contraseña);
+
+                if (resp != null) user = resp;
+
+                return user;
+            }
+        }
+
+        // POST api/<usuariosController>
+        [HttpPost]
+        public void Post([FromBody] Usuario oUsuario)
+        {
+            using (var db = new cryptoDbContext())
+            {
+                db.Usuarios.Add(oUsuario);
+                db.SaveChanges();
+            }
+        }
+
+        // PUT api/<usuariosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<usuariosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Backend/backend/Models/Movimiento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cryptoAppAPI.Models
+{
+    public partial class Movimiento
+    {
+        public long IdMov { get; set; }
+        public DateTime Fecha { get; set; }
+        public long Monto { get; set; }
+        public string Operacion { get; set; } = null!;
+        public long IdBilleteras { get; set; }
+
+        public virtual Billetera IdBilleterasNavigation { get; set; } = null!;
+    }
+}
